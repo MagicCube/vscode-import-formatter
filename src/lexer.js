ab8b36d61772fe81217776f79fbd1471f036b75d
Henry Li <henry1943@163.com>
2017-09-08 19:27:22 +0800

    Add isRequireStatement()

--- src/lexer.js
+++ src/lexer.js
@@ -12,6 +12,27 @@ function isImportStatement(rawCode) {
 }
 
 /**
+ * Returns whether the code is using require();
+ *
+ * The following codes will return true:
+ *   var a = require('a');
+ *   const b = require('b');
+ *   let c = require('c');
+ *
+ * The following code will return false:
+ *   require('a');
+ *
+ * @param {any} rawCode
+ * @returns
+ */
+function isRequireStatement(rawCode) {
+  const code = rawCode.trim();
+  return (
+    code.match(/var|const|let\s*([\w\$]+)\s*=\s*require\s*\(/)
+  );
+}
+
+/**
  * Returns whether the code is empty.
  *
  * @param {any} rawCode

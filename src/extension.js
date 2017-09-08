6dadfa73ec19d27e4b9120a0883ea8ffd7ec37b0
Henry Li <henry1943@163.com>
2017-09-08 20:47:30 +0800

    Remove comments

--- src/extension.js
+++ src/extension.js
@@ -5,8 +5,6 @@ const formatter = require('./formatter');
 // Register as Javascript formatting provider
 vscode.languages.registerDocumentFormattingEditProvider('javascript', {
   provideDocumentFormattingEdits(document) {
-    // Triggered when using 'Format Document' command,
-    // or if the user choose formatting before saving.
     return formatter.format(document);
   }
 });
